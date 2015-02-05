08caf9d78251e48105a50004761cbc17c469572b
Tommy Tang <1530234656@qq.com>
2015-02-05 15:22:00 +0800

    update

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require("express");
+var logger = require("morgan");
 var xmlBodyParser = require("./lib/xmlBodyParser").xmlBodyParser;
 var weixin = require("./lib/index");
 var app = express();
 
 app.set('port', process.env.PORT || 80);
 app.use(xmlBodyParser);
-
+app.use(logger('combined'));
 app.get('/weixin', weixin.doGet);
 app.post('/weixin', weixin.doPost);
 
