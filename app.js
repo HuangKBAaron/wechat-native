b55118488886df2a3a4db1aeff6e5578c2ba60dd
Tommy Tang <1530234656@qq.com>
2015-02-05 15:16:09 +0800

    init

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var xmlBodyParser = require("./lib/xmlBodyParser").xmlBodyParser;
+var weixin = require("./lib/index");
+var app = express();
+
+app.set('port', process.env.PORT || 80);
+app.use(xmlBodyParser);
+
+app.get('/weixin', weixin.doGet);
+app.post('/weixin', weixin.doPost);
+
+
+app.listen(app.get('port'), function () {
+    console.log('Server listening on:', app.get('port'));
+});
