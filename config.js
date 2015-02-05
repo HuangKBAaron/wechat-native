1919e4b8415fa25381bff371cfb233c94fdcd8f3
Tommy Tang <1530234656@qq.com>
2015-02-05 16:31:30 +0800

    update

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    wechat: {
+        token: 'tommy',
+        appid: 'wx00dfe0c46246cf13',
+        encodingAESKey: 'tK7jfLON3at0uTSqORQBzFU8dxrujRbt802rtJr5UEa'
+    },
+    host: {
+        port: 3001,
+        env: 'development'
+    }
+};
